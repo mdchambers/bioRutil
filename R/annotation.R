a3382a5ad3a6e719666b45e50e4063d27213f6bd
Michael Chambers <mike@mdchambers.com>
2015-06-02 19:41:22 -0700

    Several new functions

--- R/annotation.R
+++ R/annotation.R
@@ -16,3 +16,22 @@ fbToCommon <- function(){
 	map.df <- dplyr::left_join(fb.df, gn.df, by="entrez")
 	return(map.df)
 }
+
+
+#' Returns a data_frame for mapping FB CG numbers to FBgn
+#'
+#' Returns a data_frame for mapping FB CG numbers to FBgn
+#'
+#' @return A mapping data_frame
+#' @keywords annotation
+#' @export
+CG2FBgn <- function () 
+{
+    fb <- unlist(AnnotationDbi::as.list(org.Dm.eg.db::org.Dm.egFLYBASE2EG))
+    fb.df <- dplyr::data_frame(fly = names(fb), entrez = as.vector(fb))
+    cg <- unlist(AnnotationDbi::as.list(org.Dm.eg.db::org.Dm.egFLYBASECG))
+    cg.df <- dplyr::data_frame(entrez = names(cg), fbcg = as.vector(cg))
+
+    map.df <- dplyr::left_join(fb.df, cg.df, by="entrez")
+    return(map.df)
+}
--- R/chip.R
+++ R/chip.R
@@ -17,3 +17,27 @@ import.macs2.xls <- function(file){
 			enrich=xls$fold_enrichment, 
 			name=xls$name)
 }
+
+
+#' Import a MACS2 NarrowPeak file as a GRanges object
+#'
+#' Creates a GRanges object from the NarrowPeak file, taking into account switch from zero-base to one-base nomenclature. Adds and labels data as metadata.
+#'
+#' @param file Path to a NarrowPeak file for import
+#' @return A GRanges object
+#' @keywords MACS2 chip GRanges
+#' @export
+import.macs2.narrowpeak <- function(file){
+	np <- read.table(file, header=F)
+	gr <- GenomicRanges::GRanges(seqnames=np$V1,
+		ranges=IRanges::IRanges(start=np$V2+1, end=np$V3),
+		score=np$V5,
+		name=np$V4,
+		fc=np$V7,
+		log10p=np$V8,
+		log10q=np$V9,
+		relsummit=np$V10,
+		summit=np$V2+np$V10+1)
+	names(gr) <- np$V4
+	gr
+}
